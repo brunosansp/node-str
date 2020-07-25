bad3fd2a23dd31a8cc6885c7341748fbaf1813eb
Bruno Santos <brunosansp@outlook.com>
2020-07-24 22:37:19 -0300

    Add functions normalizePort e onError

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// aulda 3 balta.io curso 1972
 'user strict'
 
 const http = require('http')
@@ -6,7 +5,7 @@ const debug = require('debug')('nodestr:server')
 const express = require('express')
 
 const app = express()
-const port = 3000
+const port = normalizePort(process.env.PORT || '3000')
 app.set('port', port)
 
 const server = http.createServer(app)
@@ -18,7 +17,44 @@ const route = router.get('/', (req, res, next) => {
     version: "0.0.1"
   })
 })
-app.unsubscribe('/', route)
+app.use('/', route)
 
 server.listen(port)
+server.on('error', onError)
+
 console.log('API rodando na porta ' + port)
+
+function normalizePort(val) {
+  const port = parseInt(val, 10)
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port
+  }
+  return false
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  const bind = typeof port === 'string' ?
+    'Pipe ' + port :
+    'Pipe ' + port
+
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.')
+        process.exit(1)
+        break
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.')
+        process.exit(1)
+        break
+      default:
+        throw error
+    }
+}
