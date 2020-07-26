2e23d9c53e6aabdfe9a5aaed8d346435fe5cb341
Bruno Santos <brunosansp@outlook.com>
2020-07-26 11:56:38 -0300

    Add route product create

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
-'use strict'
+"use strict";
 
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const router = express.Router()
+const app = express();
+const router = express.Router();
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-      title: "Node Store API",
-      version: "0.0.1"
-    })
-  })
-  app.use('/', route)
+const route = router.get("/", (req, res, next) => {
+  res.status(200).send({
+    title: "Node Store API",
+    version: "0.0.2",
+  });
+});
 
-  module.exports = app
+const getAllProducts = router.get('/', (req, res, next) => {
+  res.status(200).send(req.body)
+})
+
+const create = router.post('/', (req, res, next) => {
+  res.status(201).send(req.body);
+})
+
+app.use("/", route);
+app.use('/products', create)
+
+module.exports = app;;
