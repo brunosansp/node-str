0358d2ef586221bde27ebf1847c862522f9fc59e
Bruno Santos <brunosansp@outlook.com>
2020-08-10 23:37:42 -0300

    Melhorias, separando rotas

--- src/app.js
+++ src/app.js
@@ -1,26 +1,19 @@
 "use strict";
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const router = express.Router();
 
-const route = router.get("/", (req, res, next) => {
-  res.status(200).send({
-    title: "Node Store API",
-    version: "0.0.2",
-  });
-});
+// Carraga as Rotas
+const indexRoute = require("./routes/index-route");
+const productRoute = require("./routes/product-route");
 
-const getAllProducts = router.get('/', (req, res, next) => {
-  res.status(200).send(req.body)
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const create = router.post('/', (req, res, next) => {
-  res.status(201).send(req.body);
-})
+app.use("/", indexRoute);
+app.use("/products", productRoute);
 
-app.use("/", route);
-app.use('/products', create)
-
-module.exports = app;;
+module.exports = app;
