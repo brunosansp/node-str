b1508d417d7103cb793ed12287c5e9b582bef051
Bruno Santos <brunosansp@outlook.com>
2020-08-15 16:37:01 -0300

    Add mongoose and schema from products

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose')
 
 const app = express();
 const router = express.Router();
+
+// Conecta ao banco de dados
+mongoose.connect('mongodb+srv://bruno:bruno@nodestr.tqrnr.azure.mongodb.net/nodestr?retryWrites=true&w=majority')
 
 // Carraga as Rotas
 const indexRoute = require("./routes/index-route");
 const productRoute = require("./routes/product-route");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ 
+    extended: false 
+}));
 
 app.use("/", indexRoute);
 app.use("/products", productRoute);
---
+++ src/models/products.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const schema = new Schema({
+    // Não foi colocado id pois o Schema adiciona automaticamente como _id
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    slug: { // Ex: NM_Produto: Cadeira gamer => slug: cadeira-gamer
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        required: true
+    },
+    tags: [{
+        type: String,
+        required: true
+    }]
+})
+
+module.exports = mongoose.model('Product', schema)
